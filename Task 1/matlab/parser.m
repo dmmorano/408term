683aa1c2009c2967891b4175cd7d3d8ac227c158
lancer6x <moranodylan@gmail.com>
2014-04-10 16:13:14 -0400

    data parser
    parser.m added to Task 1. Used to convert new WIS data in to old format
    for use in provided functions

---
+++ Task 1/matlab/parser.m
@@ -0,0 +1,27 @@
+function parser(fileout,filein)
+
+
+[DATE, STATION, LAT, LONG, WNDSPD, WNDDIR, USTAR, CD, WAVSTRS, Hmo, TPD, TP, TM, TM1, TM2, WAVD, SPRD, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~]...
+    = textread(filein,'%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f','headerlines',0);
+
+% Date parsing
+DATEstring = num2str(DATE);
+[YY,MM,DD,HH,mm,ss] = datevec(DATEstring,'yyyymmddHHMMSS');
+
+ID = STATION;
+YEAR = YY;
+DPTH = YY;  %Depth not record, arbitrarily used YY so matrix agrees
+DTp = TPD;
+Atp = TP;
+tmean = TM1;
+wdvmn = WAVD
+wv = WAVD;
+wsp = WNDSPD;
+wdir = WNDDIR;
+
+parsed = [ID YEAR MM DD HH LONG LAT DPTH Hmo DTp Atp tmean wdvmn wv wsp wdir];
+csvwrite(fileout,parsed)
+
+print 'Converted File Outputted'
+
+
