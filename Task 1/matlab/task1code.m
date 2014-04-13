1185b8411ce8f9f69cba07d1da1398ffea6de66c
lancer6x <moranodylan@gmail.com>
2014-04-13 13:50:55 -0400

    fixes

---
+++ Task 1/matlab/task1code.m
@@ -0,0 +1,22 @@
+% Task 1: Determine design deep water wave heights 
+% and periods for dominant directions relative to 
+% Narragansett Beach.
+
+
+%% Determine wave directions (wavedir used as reference)
+
+% Parse data in to useable format
+
+% parser('ST63079.txt','ST63079_v01.onlns')
+
+%% Plot wave direction concentrations
+wavedir('ST63079.txt')
+
+%% Gumble Distrobution
+extremeDist2_new('ST63079.txt')
+
+%% Monthly Extrema
+
+for deg = [150 180 210]
+   monthextrema('ST63079.txt',deg,30)
+end
